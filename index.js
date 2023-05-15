f6f3e734aa72e557a6fcf9227157980d83492904
Sam Peck <fullstacksam@protonmail.com>
2023-05-15 15:20:37 -0400

    get it

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution in this file!
+
+
+
+
+const returnFirstTwoDrivers = function (array){
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+}
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    function innerfuc(fare){
+        return fare * int
+    }
+
+    return innerfuc
+}
+
+
+
+
+
+const fareDoubler = (fare) => fare * 2
+const fareTripler = (fare) => fare * 3
+
+
+function selectDifferentDrivers(driverArray, theFunc ){
+
+    if (theFunc === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(driverArray)
+    }
+    else if (theFunc === returnLastTwoDrivers){
+        return returnLastTwoDrivers(driverArray)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
